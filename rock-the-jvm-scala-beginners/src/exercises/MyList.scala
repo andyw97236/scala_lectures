b08aceae6decce1951a6faef020780f15bee0959
andyw97236 <andyw97236@gmail.com>
2020-07-21 10:49:30 -0700

    mylist traits to higher order functions

--- rock-the-jvm-scala-beginners/src/exercises/MyList.scala
+++ rock-the-jvm-scala-beginners/src/exercises/MyList.scala
@@ -1,96 +1,110 @@
 package exercises
 
 abstract class MyList[+A] {
+  /*
+     head = first element of  the  list
+     tail = remainder of the list
+     isEmpty = is this list empty
+     add(int) => new list with this element added
+     toString => a string representation of the list
+   */
+
   def head: A
   def tail: MyList[A]
-  def isEmpty(): Boolean
+  def isEmpty: Boolean
   def add[B >: A](element: B): MyList[B]
   def printElements: String
   // polymorphic call
   override def toString: String = "[" + printElements + "]"
 
-  def map[B](transformer: MyTransformer[A,B]): MyList[B]
-  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B]
-  def filter(predicate: MyPredicate[A]): MyList[A]
+  // higher-order functions
+  def map[B](transformer: A => B): MyList[B]
+  def flatMap[B](transformer: A => MyList[B]): MyList[B]
+  def filter(predicate: A => Boolean): MyList[A]
 
-  //concatenation
+  // concatenation
   def ++[B >: A](list: MyList[B]): MyList[B]
 }
 
 case object Empty extends MyList[Nothing] {
   def head: Nothing = throw new NoSuchElementException
   def tail: MyList[Nothing] = throw new NoSuchElementException
-  def isEmpty(): Boolean = true
+  def isEmpty: Boolean = true
   def add[B >: Nothing](element: B): MyList[B] = new Cons(element, Empty)
-  def printElements = ""
+  def printElements: String = ""
 
-  def map[B](transformer: MyTransformer[Nothing,B]): MyList[B] = Empty
-  def flatMap[B](transformer: MyTransformer[Nothing, MyList[B]]): MyList[B] = Empty
-  def filter(predicate: MyPredicate[Nothing]): MyList[Nothing] = Empty
+  def map[B](transformer: Nothing => B): MyList[B] = Empty
+  def flatMap[B](transformer: Nothing => MyList[B]): MyList[B] = Empty
+  def filter(predicate: Nothing => Boolean): MyList[Nothing] = Empty
+
   def ++[B >: Nothing](list: MyList[B]): MyList[B] = list
 }
 
 case class Cons[+A](h: A, t: MyList[A]) extends MyList[A]{
   def head: A = h
   def tail: MyList[A] = t
-  def isEmpty(): Boolean = false
+  def isEmpty: Boolean = false
   def add[B >: A](element: B): MyList[B] = new Cons(element, this)
   def printElements: String =
-    if (t.isEmpty()) "" + h
+    if(t.isEmpty) "" + h
     else h + " " + t.printElements
 
-  def filter(predicate: MyPredicate[A]): MyList[A] =
-    if (predicate.test(h)) new Cons(h, t.filter(predicate))
+  /*
+    [1,2,3].filter(n % 2 == 0) =
+      [2,3].filter(n % 2 == 0) =
+      = new Cons(2, [3].filter(n % 2 == 0))
+      = new Cons(2, Empty.filter(n % 2 == 0))
+      = new Cons(2, Empty)
+   */
+  def filter(predicate: A => Boolean): MyList[A] =
+    if (predicate(h)) new Cons(h, t.filter(predicate))
     else t.filter(predicate)
 
-  def map[B](transformer: MyTransformer[A,B]): MyList[B] =
-    new Cons(transformer.transform(h), t.map(transformer))
+  /*
+    [1,2,3].map(n * 2)
+      = new Cons(2, [2,3].map(n * 2))
+      = new Cons(2, new Cons(4, [3].map(n * 2)))
+      = new Cons(2, new Cons(4, new Cons(6, Empty.map(n * 2))))
+      = new Cons(2, new Cons(4, new Cons(6, Empty))))
+   */
+  def map[B](transformer: A => B): MyList[B] =
+    new Cons(transformer(h), t.map(transformer))
 
+  /*
+    [1,2] ++ [3,4,5]
+    = new Cons(1, [2] ++ [3,4,5])
+    = new Cons(1, new Cons(2, Empty ++ [3,4,5]))
+    = new Cons(1, new Cons(2, new Cons(3, new Cons(4, new Cons(5)))))
+   */
   def ++[B >: A](list: MyList[B]): MyList[B] = new Cons(h, t ++ list)
-
-  def flatMap[B](transformer: MyTransformer[A, MyList[B]]): MyList[B] =
-    transformer.transform(h)  ++ t.flatMap(transformer)
+  /*
+    [1,2].flatMap(n => [n, n+1])
+    = [1,2] ++ [2].flatMap(n => [n, n+1])
+    = [1,2] ++ [2,3] ++ Empty.flatMap(n => [n, n+1])
+    = [1,2] ++ [2,3] ++ Empty
+    = [1,2,2,3]
+   */
+  def flatMap[B](transformer: A => MyList[B]): MyList[B] =
+    transformer(h) ++ t.flatMap(transformer)
 
 }
 
-trait MyPredicate[-T] {
-  def test(element: T): Boolean
-}
-
-trait MyTransformer[-A, B]{
-  def transform(elem: A): B
-}
-
-class EvenPredicate extends MyPredicate[Int]{
-  def test(a: Int): Boolean = if (a % 2 == 0) true else false
-}
-
-class StringToIntTransformer extends MyTransformer[String, Int]{
-  def transform(a: String): Int = a.toInt
-}
 
 object ListTest extends App {
-  val listOfInts: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  val cloneListOfInts: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
-  val anotherListOfInts: MyList[Int] = new Cons(4, new Cons(5, Empty))
-  val listOfStrings: MyList[String] = new Cons("a", new Cons("b", new Cons("c", Empty)))
+  val listOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val cloneListOfIntegers: MyList[Int] = new Cons(1, new Cons(2, new Cons(3, Empty)))
+  val anotherListOfIntegers: MyList[Int] = new Cons(4, new Cons(5, Empty))
+  val listOfStrings: MyList[String] = new Cons("Hello", new Cons("Scala", Empty))
 
-  println(listOfInts)
-  println(listOfStrings)
+  println(listOfIntegers.toString)
+  println(listOfStrings.toString)
 
-  println(listOfInts.map(new MyTransformer[Int,Int]{
-    override def transform(elem: Int) : Int = elem * 2
-  }))
+  println(listOfIntegers.map(_ * 2).toString)
 
-  println(listOfInts.filter(new MyPredicate[Int] {
-    override def test(elem: Int): Boolean = elem % 2 == 0
-  }))
+  println(listOfIntegers.filter(_ % 2 == 0).toString)
 
-  println((listOfInts ++ anotherListOfInts).toString)
+  println((listOfIntegers ++ anotherListOfIntegers).toString)
+  println(listOfIntegers.flatMap(elem => new Cons(elem, new Cons(elem + 1, Empty))).toString)
 
-  println(listOfInts.flatMap(new MyTransformer[Int, MyList[Int]] {
-    override def transform(elem: Int): MyList[Int] = new Cons(elem, new Cons( elem +1, Empty))
-  }).toString)
-
-  println(listOfInts == cloneListOfInts)
+  println(cloneListOfIntegers == listOfIntegers)
 }
--- rock-the-jvm-scala-beginners/src/lectures/part3fp/WhatsAFunction.scala
+++ rock-the-jvm-scala-beginners/src/lectures/part3fp/WhatsAFunction.scala
@@ -1,5 +1,44 @@
 package lectures.part3fp
 
-object WhatsAFunction {
+object WhatsAFunction extends App {
+  // use functions as first class elements
+  // problem:
+
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(element: Int): Int = element * 2
+  }
+
+  println(doubler(2))
+
+  // function types = Function1[A, B]
+  val stringToIntConverer = new Function1[String, Int] {
+    override def apply(string: String): Int = string.toInt
+  }
+
+  println(stringToIntConverer("3") + 4)
+
+  // function types Function2[A, B, R] === (A, B) => R
+
+  // ALL SCALA FUNCTIONS ARE OBJECTS
+
+  val stringConcat: ((String, String) => String) = new Function2[String, String, String] {
+    override def apply(a: String, b: String): String = a + b
+  }
+
+  println (stringConcat("a", " c"))
+
+  def sqInt = (a: Int) => {
+    a * a
+  }
+
+  def myFunc = (a: Int) =>  sqInt(a)
+
+  println(myFunc(4))
+
 
 }
+
+trait MyFunction[A, B] {
+  def apply(element: A):B
+}
+
